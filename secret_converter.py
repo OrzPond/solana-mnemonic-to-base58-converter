557dac3f580eed49027f3d8361252786f1028e9a
Supakorn R <31922687+OrzPond@users.noreply.github.com>
2022-01-24 11:16:59 +0700

    Delete secret_converter.py

--- secret_converter.py
+++
@@ -1,7 +0,0 @@
-import base58
-
-# Input your mnemonic key from phantom
-byte_array = base58.b58decode('YOUR_SECRET_KEY')
-
-json_string = "[" + ",".join(map(lambda b: str(b), byte_array)) + "]"
-print(json_string)
